807524e2035e63aa59e3170e1504a392b23312eb
jtierno <jtierno@tuenti.com>
2020-01-25 16:04:56 +0100

    Step 1: Test support

---
+++ src/test/kotlin/jonatantierno/SmokeTest.kt
@@ -0,0 +1,12 @@
+package jonatantierno
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SmokeTest {
+    @Test
+    fun simpleSillytest() {
+        assertEquals(4, 2+2);
+    }
+
+}
